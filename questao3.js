1d8efaeaefb2e7aa1d434d1a4e8ee85a7e3156bb
professor-wanderson-timoteo <wanderson_timoteo@outlook.com>
2024-03-13 16:47:01 -0400

    Questão 03 resolvida

---
+++ questao3.js
@@ -0,0 +1,10 @@
+/*
+Questão 3: Como você pode concatenar duas strings em JavaScript?
+*/
+
+let nome = "Wanderson"
+let sobrenome = "Timóteo"
+let resultado;
+
+resultado = nome +" "+ sobrenome;
+console.log(resultado);
