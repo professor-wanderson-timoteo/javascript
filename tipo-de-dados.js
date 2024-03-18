f2b70e934929cf5737e88692c66d3e17881663f4
professor-wanderson-timoteo <wanderson_timoteo@outlook.com>
2024-03-18 11:30:14 -0400

    add tipos de dados

---
+++ tipo-de-dados.js
@@ -0,0 +1,26 @@
+// var nome = "Felipe";
+// console.log(typeof(nome));
+
+// var caracter = 'F';
+// console.log(typeof(caracter));
+// var idade = 25;
+// console.log(typeof(idade));
+
+// var salario = 1.500;
+// console.log(typeof(salario));
+
+// var ligada =  false;
+// console.log(typeof(ligada));
+
+// var desligada =  true;
+// console.log(typeof(desligada));
+
+// var a =  10;
+//console.log(typeof(a));
+// var b =  "10";
+//console.log(typeof(b));
+
+// console.log(a == b);
+// console.log(a === b);
+
+console.log(true > false);
